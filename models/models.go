3041887483db4939d990c4108965d641da8f4d38
Atul Ranjan <atulranjan789@gmail.com>
2023-06-06 12:39:44 +0530

    import package primitve

--- models/models.go
+++ models/models.go
@@ -1,8 +1,8 @@
-package model
+package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type user struct {
+type User struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	UserName string             `json:"username,omitempty"`
 	Password string             `json:"password,omitempty"`
