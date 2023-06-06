dc739eda99b5f1064525ce0d8d40ca83eec2c515
Atul Ranjan <atulranjan789@gmail.com>
2023-06-06 13:27:24 +0530

    create a server

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/atul-007/user-login/router"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	r := router.Router()
+	fmt.Println("server is getting started")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000...")
+}
